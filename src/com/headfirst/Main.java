2a80b78815fd80d361899c28a7a50a80c19199dc
zhaoxudong <1097483508@qq.com>
2021-08-17 14:47:07 +0800

    个人实验和测试

--- src/com/headfirst/Main.java
+++ src/com/headfirst/Main.java
@@ -2,7 +2,24 @@ package com.headfirst;
 
 public class Main {
 
+    static class superClass{
+        static {
+            System.out.println("super load");
+        }
+    }
+
+    static class SubClass extends superClass{
+        static {
+            System.out.println("sub load");
+        }
+
+        public static void test(){
+            System.out.println("sdsad");
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        SubClass.test();
     }
 }
